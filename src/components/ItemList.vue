6c71bd0da15221fb8e3daaebc668b72e7363d6c2
masa13535 <51148171+masa13535@users.noreply.github.com>
2021-05-26 00:22:01 +0900

    UPDATE 商品のリストデータを表示する

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -1,5 +1,13 @@
 <template>
-    <div>ItemList</div>
+    <div>
+        <div>ItemList</div>
+        <div v-for="item in items" :key="item.name">
+            <div class="item">
+                <div class="name">名前: {{ item.name }}</div>
+                <div class="price">{{ item.price }} 円</div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
